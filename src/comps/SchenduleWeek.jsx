16a3268aef2002eace8f6f8f3405eb443c679e7e
jonatanc04 <jcorcolesg98@gmail.com>
2024-11-30 09:12:37 +0100

    PUESTA COMÚN

--- src/comps/SchenduleWeek.jsx
+++ src/comps/SchenduleWeek.jsx
@@ -2,16 +2,24 @@ import { useTranslation } from 'react-i18next';
 import GameRow from "./GameRow";
 
 export default function SchenduleWeek({ weekNumber, weekData }) {
-
   const { t } = useTranslation();
+
+  const isNumber = (value) => !isNaN(parseInt(value));
 
   return (
     <div className="schendule m-4 p-4 col-12 d-flex flex-column flex-lg-row flex-wrap align-items-center justify-content-center">
-      <h3 className="col-12 text-center">{t('week')} {weekNumber}</h3>
+      {isNumber(weekNumber) ? (
+        <h3 className="col-12 text-center">{t('week')} {weekNumber}</h3>
+      ) : (
+        <h3 className="bot-gap col-12 text-center">{t(weekNumber)}</h3>
+      )}
+
       {Object.entries(weekData).map(([groupNumber, gameData]) => (
-        <div className='col-12 col-lg-6'>
-          <h4 className="col-12 text-center">{t('group')} {groupNumber}</h4>
-          <div key={groupNumber} className={`group${groupNumber} col-12 d-flex flex-column`}>
+        <div className='col-12 col-lg-6' key={groupNumber}>
+          {isNumber(weekNumber) && (
+            <h4 className="col-12 text-center">{t('group')} {groupNumber}</h4>
+          )}
+          <div className={`group${groupNumber} col-12 d-flex flex-column`}>
             {Object.entries(gameData).map(([gameKey, gameInfo]) => (
               <GameRow key={gameKey} gameInfo={gameInfo}></GameRow>
             ))}
@@ -19,5 +27,5 @@ export default function SchenduleWeek({ weekNumber, weekData }) {
         </div>
       ))}
     </div>
-  )
+  );
 }

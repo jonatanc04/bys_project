95e41a1b1a23a4360cedd8c5e3157b046952440b
jonatanc04 <jcorcolesg98@gmail.com>
2025-02-03 19:18:23 +0100

    INSCRIPCIÓN Y NUEVA NORMATIVA

--- src/pages/inscription.jsx
+++ src/pages/inscription.jsx
@@ -2,12 +2,18 @@ import { useTranslation } from 'react-i18next';
 import '../assets/styles/Inscriptions.css';
 
 export default function Inscription() {
-
   const { t } = useTranslation();
+  const googleFormUrl = "https://docs.google.com/forms/d/e/1FAIpQLSeUxft2_eVQFJJTSJQurSptYx39wdiVW_QvUzGGrSmV9hwurA/viewform";
 
   return (
     <div className="inscription fade-in container d-flex flex-column align-items-center justify-content-center">
-      <h3 className='p-3 m-3 col-12 text-center'>{t('inscriptions_no')}</h3>
+      <iframe 
+        src={googleFormUrl} 
+        width="100%" 
+        height="800px" 
+        style={{ border: 'none', marginTop: '20px' }} 
+        title="Google Form"
+      ></iframe>
     </div>
-  )
+  );
 }
